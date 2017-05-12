fd804baa6c3b8321bc15f937f60dca65d8d557e6
Delevin Zhong <502202587@qq.com>
2017-05-12 13:31:11 +0800

    Create pushToBitbucket.bat

---
+++ pushToBitbucket.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set path=%path%;C:/Program Files (x86)/Git/bin
+
+echo "This script will push mutiple projects to Bitbucket automatically."
+
+echo "Please input the path: "
+
+set /p dir=
+
+for /f "delims=," %%j in (%dir%\filelist.txt) do (
+cd %dir%\%%j
+
+git init 
+git config http.sslVerify false
+rem git pull origin master
+git add --all
+git commit -m "Push the codes to Bitbucket"
+git remote add origin ssh://git@bitbucket.service.dev/dede/%%j.git
+git push origin master
+)
+cd %dir%
